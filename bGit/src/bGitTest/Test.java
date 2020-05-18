96f31d3bd04ac3f4d723c42d60d125cadb1e6dd4
user2 <user2@DESKTOP-CVTM9PG>
2020-05-18 17:15:00 +0900

    fourth_commit

--- bGit/src/bGitTest/Test.java
+++ bGit/src/bGitTest/Test.java
@@ -4,6 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("안뇽?");
 		System.out.println("안녕 반가워 난 조원이ㅑㅇ");
+
+		System.out.println("YYYYYYYYYYYYY");
+
 		System.out.println("XXXXXXXXXXX");
 	}
 }
